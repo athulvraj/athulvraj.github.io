78e111746b6160d2ce6a986f5b7386be3c5dded3
athulvraj <athulvraj@gmail.com>
2015-06-23 01:08:02 +0530

    Update portfolio-main.js

--- js/app/portfolio-main.js
+++ js/app/portfolio-main.js
@@ -10,7 +10,9 @@ function myController($scope){
    phone:"+91 8939123432",
 
    address:"Correspondence address<br>WF3,Ambrosiya Apts<br>Kumaran Nagar	Chennai-60019",
-   objectieve:"To obtain a long term career with an organization which has a strong background that provides good opportunity for enhancement of professional and personal skills. To be a continuous winner by being a perfect team player and adding value to the organization in all domains possible."
+   objectieve:"To obtain a long term career with an organization which has a strong background that provides good opportunity for enhancement of professional and personal skills. To be a continuous winner by being a perfect team player and adding value to the organization in all domains possible.",
+  heading.head_objectieve:"CAREER OBJECTIVE" 
+  
  }
   
   
