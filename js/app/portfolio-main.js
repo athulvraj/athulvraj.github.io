07947911ce919227551c7677742e1ad75cb252ec
athulvraj <athulvraj@gmail.com>
2015-06-22 23:31:12 +0530

    Update portfolio-main.js

--- js/app/portfolio-main.js
+++ js/app/portfolio-main.js
@@ -1,1 +1,17 @@
-
+var myapp=angular.module("myApp",[]); 
+myApp.controller("myController",
+function myController($scope){
+ $scope.heading=[{
+   name:"ATHUL V RAJ"
+ } ,{
+ title:"FRONT END WEB DEVELOPER"
+ },{
+   email:"athulvraj@gmail.com"
+ },{
+   phone:"+91 8939123432"
+ },{
+   address:"Correspondence address<br>WF3,Ambrosiya Apts<br>Kumaran Nagar	Chennai-60019"
+ }]
+  
+  
+});
